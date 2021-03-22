f4b0089eaeed5d0c7912b21992597062ec227bfe
hnjoku <hnjoku@parkwayprojects.com>
2021-03-22 11:58:45 +0100

    Express REST API with mongoose part 1

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leadersRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/hFirstData';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('Connected correctly to server');
+},(err) => {console.log(err)});
+
 var app = express();
 
 // view engine setup
